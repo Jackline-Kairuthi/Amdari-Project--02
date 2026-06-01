0f1886c2950dc564a5269533c7c322c71485d32a
Jackline Kairuthi <jackiekairuthi@gmail.com>
2026-06-01 22:06:28 +0100

    Fix backend.tf: remove unsupported use_lockfile argument

--- infra/us-west2/backend.tf
+++ infra/us-west2/backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "s3" {
-    bucket       = "sentinelpay-tf-state-west2"
-    key          = "infra/us-west-2/terraform.tfstate"
-    region       = "us-west-2"
-    use_lockfile = true
+    bucket         = "sentinelpay-terraform-state"
+    key            = "us-west2/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "sentinelpay-terraform-locks"
+    encrypt        = true
   }
 }
+
