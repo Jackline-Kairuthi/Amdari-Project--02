76649b3e782a1aaa07f7002ebc4134fefaa1fd31
Jackline Kairuthi <jackiekairuthi@gmail.com>
2026-06-02 16:39:19 +0100

    Fix backend key path for Terraform state

--- infra/us-west2/backend.tf
+++ infra/us-west2/backend.tf
@@ -1,11 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "sentinelpay-tf-state-west2"
-    key            = "us-west2/terraform.tfstate"
-    region         = "us-west-2"
+    bucket = "sentinelpay-tf-state-west2"
+    key    = "infra/us-west-2/terraform.tfstate"
+    region = "us-west-2"
     dynamodb_table = "sentinelpay-terraform-locks"
-    encrypt        = true
+    encrypt = true
   }
 }
+
 
 
